7df830a7a8fe5d7dced4d63b23443bc0d39e7f2d
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-08-11 09:32:48 +0530

    import java.util.*; public class Inputtext {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String msg=sc.nextLine(); // To read string as input         System.out.println(msg);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Inputtext
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String msg=sc.nextLine(); // To read string as input
+        System.out.println(msg);
+    }
+}

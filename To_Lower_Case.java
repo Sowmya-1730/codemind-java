708a3a6e7e91d0df80a7fa5f205dc9b6cbf2f277
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-08-26 10:26:27 +0530

    import java.util.*; public class Lower {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.next();         System.out.printf("%s",s.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Lower
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.printf("%s",s.toLowerCase());
+    }
+}

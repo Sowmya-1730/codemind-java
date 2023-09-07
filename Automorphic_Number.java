eed15a05a290a7aa2922c5018591203df7dd715f
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-09-07 10:54:35 +0530

    import java.util.*; public class Automorphic {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int s=n*n;         String s1=Integer.toString(n);         String s2=Integer.toString(s);         if(s2.endsWith(s1))         {             System.out.println("Automorphic Number");         }         else         {             System.out.println("Not an Automorphic Number");         }     } }

---
+++ Automorphic_Number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Automorphic
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=n*n;
+        String s1=Integer.toString(n);
+        String s2=Integer.toString(s);
+        if(s2.endsWith(s1))
+        {
+            System.out.println("Automorphic Number");
+        }
+        else
+        {
+            System.out.println("Not an Automorphic Number");
+        }
+    }
+}

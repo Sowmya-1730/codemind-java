d4ba2ddb524fb005f441dd2ec49025e401096c7b
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-10-04 15:15:48 +0530

    import java.util.Scanner; public class Smaller {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             int cnt=0;             for(int j=0;j<n;j++)             {                 if(arr[i]>arr[j])                 {                     cnt++;                 }             }             System.out.print(cnt+" ");         }     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Smaller
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int cnt=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    cnt++;
+                }
+            }
+            System.out.print(cnt+" ");
+        }
+    }
+}
